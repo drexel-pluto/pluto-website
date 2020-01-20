3abc19aa09109dc0e98ee16da7e1d2a3cc1258e2
Mike Carbone <mfcbone2@gmail.com>
2020-01-20 17:42:11 -0500

    :rocket: Email send hooked into API

--- globals.js
+++ globals.js
@@ -1,5 +1,7 @@
 const globals = {
-    website: "https://www.plutosocial.io"
+    website: "https://www.plutosocial.io",
+    api: "https://drexel-pluto.herokuapp.com",
+    // api: "http://localhost:3555"
 }
 
 export default globals;
--- pages/api/email/[email].js
+++ pages/api/email/[email].js
@@ -1,5 +1,21 @@
+import globals from '../../../globals'
+import fetch from 'isomorphic-unfetch';
+const apiUrl = globals.api;
+
 export default (req, res) => {
-    const email = req.query.email;
-    console.log('Email received. POST to API here.');
-    res.status(200).json({ email });
+    ( async () => {
+        try {
+            const data = { email: req.query.email }
+            const payload = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            }
+            const response = await fetch(`${apiUrl}/api/public/email/save`, payload);
+            const emailSave = await response.json();
+            res.status(200).json({ email: emailSave });
+        } catch (err) {
+            res.status(500).json({ err });
+        }
+    })();
 }
